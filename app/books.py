ac0081389241872394cc54b3faf9aa73f6031aa1
Felipe Cordeiro Pires Magalhães <fcpmagalhaes@hotmail.com>
2020-10-17 22:59:52 -0300

    create create, read, and delete methods

--- app/books.py
+++ app/books.py
@@ -1,8 +1,32 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, current_app, request
+from .model import Book
+from .serealizer import BookSchema
 
 bp_books = Blueprint('books', __name__)
 
-@bp_books.route('/mostrar', methods=['GET'])
 
+@bp_books.route('/mostrar', methods=['GET']) 
 def mostrar():
-  ...
+  # result = Book.query.all()
+  # return BookSchema(many=True).jsonify(result), 200
+  bs = BookSchema(many=True)
+  result = Book.query.all()
+  return bs.jsonify(result), 200
+
+
+@bp_books.route('/cadastrar', methods=['POST']) 
+def cadastrar():
+  bs = BookSchema()
+  book, error = bs.load(request.json)
+  current_app.db.session.add(book)
+  current_app.db.session.commit()
+
+  return bs.jsonify(book), 201
+
+
+@bp_books.route('/deletar/<identificador>', methods=['DELETE']) 
+def deletar(identificador):
+  Book.query.filter(Book.id == identificador).delete()
+  current_app.db.session.commit()
+
+  return jsonify('Deletado')
